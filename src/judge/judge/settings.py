833e9a40fcd354beea023b1beab4ffaf543d9322
wdxfairy <sjtu.wdxfairy@gmail.com>
2015-12-21 21:47:13 +0800

    create config app

--- src/judge/judge/settings.py
+++ src/judge/judge/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'judgesys.apps.JudgesysConfig',
 )
 
 MIDDLEWARE_CLASSES = (
