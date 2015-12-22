662d9ce1ec057135e07f61db9a05fff7051a89bc
tangyin <tangyin@xindong.com>
2015-12-22 19:22:37 +0800

    add user profile one one to django user

--- src/judge/account/models.py
+++ src/judge/account/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+#conding=utf-8
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    real_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=11, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.real_name
